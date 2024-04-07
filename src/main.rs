c5163d732cd563983d26e538d74281cdf89fb1e7
tenado <690784374@qq.com>
2024-04-07 14:55:58 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
 fn main() {
-    println!("Hello, world!");
+    //  cargo run -- name 111.txt
+    // 获取参数
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("{}{}", query, file_path);
+
+    // 读取文件内容
+    let content = fs::read_to_string(file_path).expect("文件读取");
+    println!("text content:\n{content}")
+
 }
