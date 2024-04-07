075a45446fd74a33c34906e671d06109b3ac18ee
tenado <690784374@qq.com>
2024-04-07 16:37:32 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     // 获取参数
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("error: {}", err);
+        eprintln!("error: {}", err);
         process::exit(1);
     });
 
     // 读取文件内容
     if let Err(e) = rgrep::run(config) {
-        println!("error:{}", e);
+        eprintln!("error:{}", e);
         process::exit(1);
     }
 }
